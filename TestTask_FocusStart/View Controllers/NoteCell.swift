cebf78c6a1c234171084ba3f18880e008666f3f9
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-03-18 14:17:16 +0600

    Добавлено: вид ячейки заметки и подготовлен вид вида списка заметок

---
+++ TestTask_FocusStart/View Controllers/NoteCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteCell.swift
+//  TestTask_FocusStart
+//
+//  Created by Zufar Suleimanov on 18.03.2021.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
