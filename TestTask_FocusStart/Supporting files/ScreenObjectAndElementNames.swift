1a0d079d57c8cf22209cb5396f7c6df3b3bea386
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-03-18 14:21:57 +0600

    Добавлено: файл помощник для хранения идентификаторов объектов

---
+++ TestTask_FocusStart/Supporting files/ScreenObjectAndElementNames.swift
@@ -0,0 +1,24 @@
+//
+//  ScreenObjectAndElementNames.swift
+//  TestTask_FocusStart
+//
+//  Created by Zufar Suleimanov on 18.03.2021.
+//
+
+import Foundation
+
+enum Storyboards: String {
+    case notes = "Notes"
+}
+
+enum ViewControllers: String {
+    case notes = "Notes"
+}
+
+enum Cells : String {
+    case noteCell = "NoteCell"
+}
+
+enum XIBs : String {
+    case noteCell = "NoteCell"
+}
