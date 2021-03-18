c961d562ba9eba661eef9703d7a15030cf6b78ef
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-03-18 13:46:26 +0600

    Первичная подготовка под MVC

---
+++ TestTask_FocusStart/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask_FocusStart
+//
+//  Created by Zufar Suleimanov on 18.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
